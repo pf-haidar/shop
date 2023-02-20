e39dae1bfb6e68934b6fc72a82b82ed7e039f425
pf-haidar <pedrohfilho.dev@gmail.com>
2023-02-20 10:42:08 -0300

    feat state provider example

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shop/pages/counter_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/providers/counter.dart';
 import 'package:shop/utils/app_routes.dart';
 
 void main() {
